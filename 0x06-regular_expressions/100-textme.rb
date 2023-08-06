6e37384e0ab8579a43d37d30eb178b0ce37123c6
Youssef <youssefkaramedu@gmail.com>
2023-08-06 05:04:51 -0700

    Fix plus sign error

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(\bfrom:\K\S+(?=\])|\bto:\+\K\w+|\bflags:\K\S+(?=\]))/).join
+puts ARGV[0].scan(/(\bfrom:\K\S+(?=\])|\bto:\+\K?(\w+)|\bflags:\K\S+(?=\]))/).join
