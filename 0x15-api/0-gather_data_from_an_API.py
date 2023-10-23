3171454e4034792491b19ed6ac84a8ad76a0f27a
Youssef <youssefkaramedu@gmail.com>
2023-10-23 15:35:14 -0700

    Add style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,6 +3,7 @@
 import requests
 import sys
 
+
 def get_employee_todo_progress(employee_id):
     base_url = 'https://jsonplaceholder.typicode.com'
     employee_url = f'{base_url}/users/{employee_id}'
@@ -25,9 +26,11 @@ def get_employee_todo_progress(employee_id):
     done_tasks = [task for task in todos_data if task['completed']]
     num_done_tasks = len(done_tasks)
 
-    print(f"Employee {employee_name} is done with tasks({num_done_tasks}/{total_tasks}):")
+    print(f"Employee {employee_name} is done\
+            with tasks({num_done_tasks}/{total_tasks}):")
     for task in done_tasks:
         print(f"\t{task['title']}")
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
